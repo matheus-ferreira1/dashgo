dead33f4473b2c1786560d1dbed1cc598099c3e2
Matheus Ferreira <matheus.fer33@gmail.com>
2022-04-04 19:16:22 -0300

    componente header finalizado

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Icon, Input, Text } from "@chakra-ui/react";
-import { RiSearchLine } from 'react-icons/ri'
+import { Avatar, Box, Flex, HStack, Icon, Input, Text } from "@chakra-ui/react";
+import { RiNotificationLine, RiSearchLine, RiUserAddLine } from 'react-icons/ri'
 
 export default function Header() {
   return (
@@ -46,6 +46,35 @@ export default function Header() {
           />
 
           <Icon as={RiSearchLine} fontSize="20" />
+       </Flex>
+
+       <Flex
+         align="center"
+         ml="auto"
+       >
+         <HStack 
+           spacing="8"
+           mx="8"
+           pr="8"
+           py="1"
+           color="gray.300"
+           borderRightWidth={1}
+           borderColor="gray.700"
+         >
+            <Icon as={RiNotificationLine} fontSize="20" />
+            <Icon as={RiUserAddLine} fontSize="20" />
+          </HStack>
+
+         <Flex align="center">
+            <Box mr="4" textAlign="right">
+               <Text>Matheus Ferreira</Text>
+               <Text color="gray.300" fontSize="small">
+                  matheus.fer33@gmail.com
+               </Text>
+            </Box>
+
+            <Avatar size="md" name="Matheus Ferreira" src="https://github.com/matheus-ferreira1.png" />
+         </Flex>
        </Flex>
     </Flex>
   )
