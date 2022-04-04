359c88447a394f82d2c920ae0150e5b651f6c5a7
Matheus Ferreira <matheus.fer33@gmail.com>
2022-04-04 18:44:20 -0300

    componentizacao do input para ser reutilizado

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
-import { Button, Flex, FormControl, FormLabel, Input, Stack } from "@chakra-ui/react";
+import { Button, Flex, Stack } from "@chakra-ui/react";
+import { Input } from "../components/Form/Input";
 
 
-export default function Home() {
+export default function SignIn() {
   return (
     <Flex 
       w="100vw" 
@@ -19,37 +20,8 @@ export default function Home() {
           flexDirection="column"
         >
           <Stack spacing="4">
-            <FormControl>
-              <FormLabel htmlFor="email">E-mail</FormLabel>
-              <Input 
-                name="email" 
-                type="email"
-                id="email"
-                focusBorderColor="pink.500"
-                bgColor="gray.900"
-                variant="filled"
-                _hover={{
-                  bgColor: "gray.900"
-                }}
-                size="lg"
-              />
-            </FormControl>
-            
-            <FormControl>
-              <FormLabel htmlFor="password">Senha</FormLabel>
-              <Input 
-                name="password" 
-                type="password" 
-                id="password"
-                focusBorderColor="pink.500"
-                bgColor="gray.900"
-                variant="filled"
-                _hover={{
-                  bgColor: "gray.900"
-                }}
-                size="lg"
-              />
-            </FormControl>
+            <Input name="email" type="email" label="E-mail" />
+            <Input name="password" type="password" label="Senha" />
           </Stack>
 
           <Button type="submit" mt="6" colorScheme="pink" size="lg">Entrar</Button>
