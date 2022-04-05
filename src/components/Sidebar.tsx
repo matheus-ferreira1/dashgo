cf09b71a08ce045530560a9c56a7d04f020c99bc
Matheus Ferreira <matheus.fer33@gmail.com>
2022-04-04 21:07:45 -0300

    criação componente sidebar

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,11 @@
+import { Box, Stack } from "@chakra-ui/react";
+
+export default function Sidebar() {
+  return (
+    <Box as="aside" w="64" mr="8">
+       <Stack spacing="12" align="flex-start">
+
+       </Stack>
+    </Box>
+  )
+}
