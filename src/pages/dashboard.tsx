2e8d70fcff999331a9240ad735a633faa97d1d6c
Matheus Ferreira <matheus.fer33@gmail.com>
2022-04-07 15:04:29 -0300

    criacao da pagina e tabela de usuarios

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -9,61 +9,59 @@ const Chart = dynamic(() => import('react-apexcharts'), {
    ssr: false,
 })
 
-
+const options = {
+  chart: {
+    toolbar: {
+      show: false,
+    },
+    zoom: {
+      enabled: false,
+    },
+    foreColor: theme.colors.gray[500],
+  },
+  grid: {
+    show: false,
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  tooltip: {
+    enabled: false,
+  },
+  xaxis: {
+    type: 'datetime',
+    axisBorder: {
+      color: theme.colors.gray[600]
+    },
+    axisTicks: {
+      color: theme.colors.gray[600]
+    },
+    categories: [
+      '2021-03-18T00:00:00.000Z',
+      '2021-03-19T00:00:00.000Z',
+      '2021-03-20T00:00:00.000Z',
+      '2021-03-21T00:00:00.000Z',
+      '2021-03-22T00:00:00.000Z',
+      '2021-03-23T00:00:00.000Z',
+      '2021-03-24T00:00:00.000Z',
+    ]
+  },
+  fill: {
+    opacity: 0.3,
+    type: 'gradient',
+    gradient: {
+      shade: 'dark',
+      opacityFrom: 0.7,
+      opacityTo: 0.3,
+    }
+  }
+}
 
 const series = [
-   { name: "series 1", data: [31, 120, 10, 28, 61, 18, 131] }
+   { name: "series1", data: [31, 120, 10, 28, 51, 57, 108 ] }
 ];
 
 export default function Dashboard() {
-
-   const options = {
-      chart: {
-        toolbar: {
-          show: false
-        },
-        zoom: {
-          enabled: false
-        },
-        foreColor: theme.colors.gray[500],
-      },
-      grid: {
-        show: false
-      },
-      dataLabels: {
-        enabled: false
-      },
-      tooltip: {
-        enabled: false
-      },
-      xaxis: {
-        type: "datetime",
-        axisBorder: {
-          color: theme.colors.gray[600]
-        },
-        axisTicks: {
-          color: theme.colors.gray[600]
-        },
-        categories: [
-          '2021-11-20T00:00:00.000Z',
-          '2021-11-21T00:00:00.000Z',
-          '2021-11-22T00:00:00.000Z',
-          '2021-11-23T00:00:00.000Z',
-          '2021-11-24T00:00:00.000Z',
-          '2021-11-25T00:00:00.000Z',
-        ]
-      },
-      fill: {
-        opacity: 0.3,
-        type: 'gradient',
-        gradient: {
-          shade: 'dark',
-          opacityFrom: 0.7,
-          opacityTo: 0.3,
-        }
-      }
-    }
-
 
   return (
     <Flex direction="column" h="100vh">
